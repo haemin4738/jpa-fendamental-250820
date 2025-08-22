6c4c61b08f489f7c5639f7435538ff36e2c7531c
haemin4738 <leeheamin12@gmail.com>
2025-08-22 09:07:57 +0900

    BaseInitData Self

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -4,28 +4,33 @@ package com.back.global.initData;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @RequiredArgsConstructor
 @Configuration
 public class BaseInitData {
+    @Autowired
+    @Lazy
+    private BaseInitData self;
+
     private final PostService postService;
-    private int callCount = 0;
 
     @Bean
     ApplicationRunner baseInitDataApplicationRunner() {
         return args -> {
-            work1();
-            work2();
-
-            callCount++;
+            self.work1();
+            self.work2();
         };
     }
 
+    @Transactional
     void work1() {
         if (postService.count() > 0) return;
 
@@ -40,6 +45,7 @@ public class BaseInitData {
         System.out.println("기본 데이터가 초기화 되었습니다.");
     }
 
+    @Transactional(readOnly = true)
     void work2() {
         Optional<Post> opPost = postService.findById(1);
         // SELECT * FROM post WHERE id = 1;
